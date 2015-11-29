7b5bbe32f233d75e3400457908b41d7b3fe5dbf1
chaoc <531236305@qq.com>
2015-11-29 12:54:00 +0800

    添加了服务脚本

---
+++ rsassistantservice.py
@@ -0,0 +1,26 @@
+#-*-encoding:utf-8-*-
+
+import win32serviceutil
+import win32service
+import win32event
+import threading
+
+from rsassistant import sign_service
+
+
+class RSAssistantService(win32serviceutil.ServiceFramework):
+
+    def __init__(self, args):
+        super().__init__(args)
+        self.hWaitStop = win32event.CreateEvent(None, 0, 0, None)
+
+    def SvcStop(self):
+        self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
+        win32event.SetEvent(self.hWaitStop)
+
+    def SvcDoRun(self):
+        t = threading.Thread(target=sign_service)
+        t.setDaemon(True)
+        t.start()
+
+        win32event.WaitForSingleObject(self.hWaitStop, win32event.INFINITE)
